6c0809af32af41f0e50abd9a2e3d20c8ed48048a
Luciano <regueraluciano@gmail.com>
2022-07-16 12:40:58 -0300

    no estaba la instrucción de cerrar archivo, se cierra archivo

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -2,11 +2,14 @@ from datetime import date
 
 
 file = open('archivo.txt', 'w+')
-
 file.write('Hola mi nombre es Luciano\n')
 file.write(f'Hoy es: {date.today()}' )
+file.close()
+
 
 file = open('archivo.txt', 'r')
 
 for line in file.readlines():
     print(line)
+
+file.close()
